bfac93e29c210c05274713a1220a42158f3fe5de
reem-hazim <reem.hazim02@gmail.com>
2021-03-08 23:21:01 +0400

    Added static folder

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/static')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
