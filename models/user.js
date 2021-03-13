b5b1e14a52c0b1991a21dd122a722237aeb92179
reem-hazim <reem.hazim02@gmail.com>
2021-03-13 21:40:20 +0400

    Updated user database schema

--- models/user.js
+++ models/user.js
@@ -11,6 +11,49 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	phoneNumber: {
+		type: String,
+		default: ""
+	},
+	paymentDetails:{
+		cardNumber: {
+			type: Number,
+		},
+		CVV: {
+			type: Number,
+		},
+		expiryDate: {
+			type: Date,
+		},
+		billingAddress: {
+			address1: {
+				type: String,
+			},
+			address2: {
+				type: String,
+			},
+			emirate: {
+				type: String,
+				lowercase: true,
+				enum: ['dubai', 'abu dhabi', 'ajman', 'sharjah', 'ras al khaimah', 'umm al quwain', 'fujairah'],
+			}
+		}
+	},
+	shippingAddress: {
+		address1: {
+			type: String,
+			default: "",
+		},
+		address2: {
+			type: String,
+		},
+		emirate: {
+			type: String,
+			lowercase: true,
+			enum: ['dubai', 'abu dhabi', 'ajman', 'sharjah', 'ras al khaimah', 'umm al quwain', 'fujairah'],
+			default: "abu dhabi",
+		}	
+	}
 });
 
 const User = mongoose.model('User', userSchema);
