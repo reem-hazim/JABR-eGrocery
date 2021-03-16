fa49b62a16271faabca354291958ee0cb9be93dd
reem-hazim <reem.hazim02@gmail.com>
2021-03-16 14:48:51 +0400

    Added order schema

---
+++ models/order.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+	totalPrice: {
+		type: Number,
+		min: 0,
+	},
+	products: [{
+		product: {
+			type: Schema.Types.ObjectId,
+			ref: 'Product'
+		},
+		quantity: {
+			type: Number,
+			min: 1,
+		}
+	}],
+	orderType: {
+		type: String,
+		enum: ['pickup', 'delivery'],
+	},
+	delivery: {
+		time: {
+			type: Date,
+		},
+		shippingStatus: {
+			type: String,
+			enum: ['confirmed', 'shipped', 'outForDelivery', 'delivered'],
+		},
+		shippingAddress: {
+			type: Schema.Types.ObjectId,
+			ref: 'User'
+		},
+	},
+	pickupTime:{
+		type: String,
+	}
