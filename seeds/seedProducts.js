0b350694337931287428ae61101829e976f9e9a4
jhongover9000 <57396665+jhongover9000@users.noreply.github.com>
2021-04-27 13:12:27 +0400

    Update seedProducts.js

--- seeds/seedProducts.js
+++ seeds/seedProducts.js
@@ -154,7 +154,7 @@ const seedProducts = [
 	},
 	{
 		image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1598299038-p427419-av-01-zoom.jpg",
-		name: "The Ordinary Hyaluroic Acid",
+		name: "The Ordinary Hyaluronic Acid",
 		price: 29.00,
 		defWeight: "",
 		qtyAvailable: 42,
